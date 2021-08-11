6bd048eb6735995d7e8890b9321b504ce2c95af9
zhangshanwen <huichiroudeyu@163.com>
2021-08-11 15:30:36 +0800

    feat:     1.first commit

--- server/servers/images.go
+++ server/servers/images.go
@@ -16,6 +16,26 @@ type imagesServer struct {
 	pb.UnimplementedImageServer
 }
 
+func (s *imagesServer) ImageSearch(ctx context.Context, in *pb.ImageSearchRequest) (reply *pb.ImageSearchReply, err error) {
+	args := filters.Args{}
+	for _, v := range in.Filters {
+		args.Add(v.Key, v.Value)
+	}
+	option := types.ImageSearchOptions{}
+	if err = copier.Copy(&option, in); err != nil {
+		return
+	}
+	res, err := d.Docker.ImageSearch(ctx, in.Term, option)
+	if err != nil {
+		log.Printf("get images list err:%v", err)
+		return
+	}
+	reply = &pb.ImageSearchReply{}
+	if err = copier.Copy(&reply.List, &res); err != nil {
+		return
+	}
+	return
+}
 func (s *imagesServer) ImageList(ctx context.Context, in *pb.ImageListRequest) (reply *pb.ImageListReply, err error) {
 	args := filters.Args{}
 	for _, v := range in.Filters {
